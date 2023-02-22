2265753764d3f156391c05a4689ce2e1a3f7c23e
How Bizarre <howbizarre@gmail.com>
2023-02-22 10:58:22 +0200

    ADD: Router is ready

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,4 @@ import "bootstrap/scss/bootstrap.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const app = createApp(App).use(router);
-app.mount("#app")
-//router.isReady().then(() => app.mount("#app"));
+router.isReady().then(() => app.mount("#app"));
