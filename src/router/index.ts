c97a852a032cda7817d8ad8975ce61d0b9f90af0
How Bizarre <howbizarre@gmail.com>
2023-02-22 16:01:48 +0200

    UPD: BASE URL

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const baseUrl = import.meta.env.BASE_URL;
+const baseUrl = import.meta.env.VITE_BUILD_ADDRESS;
 
 export const routes = [
   {
-    path: `${baseUrl}`,
+    path: `${baseUrl}/`,
     component: () => import("@/layouts/Default.vue"),
     children: [
       { path: "", name: "Home", component: () => import("@/views/Home.vue") }
     ],
   },
   {
-    path: `${baseUrl}about`,
+    path: `${baseUrl}/about`,
     component: () => import("@/layouts/Page.vue"),
     children: [
       { path: "", name: "About", component: () => import("@/views/About.vue") }
     ],
   },
   {
-    path: `${baseUrl}contacts`,
+    path: `${baseUrl}/contacts`,
     component: () => import("@/layouts/Page.vue"),
     children: [
       { path: "", name: "Contacts", component: () => import("@/views/Contacts.vue") },
