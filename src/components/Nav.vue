a7a317078169c597a05beaa96d1ad2181b2c6db8
How Bizarre <howbizarre@gmail.com>
2023-02-23 12:15:44 +0200

    UPD

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <nav class="navbar navbar-expand-lg mb-4 border-bottom">
-      <router-link :to="routes[0].path" class="navbar-brand d-flex align-items-center text-dark text-decoration-none">
+      <router-link :to="routes[0].path" class="navbar-brand d-flex align-items-center text-decoration-none">
         <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 0 261.76 226.69">
           <path d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z" fill="#41b883" />
           <path d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z" fill="#34495e" />
