a36c96f7433a9f561408a986e0d1b758041b6c8d
How Bizarre <howbizarre@gmail.com>
2023-03-21 17:14:28 +0200

    ADD: LogRocket

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <RouterView />
 </template>
 
+<script lang="ts" setup>
+import LogRocket from 'logrocket';
+LogRocket.init(`${import.meta.env.VITE_LOGROCKET_ID}`);
+</script>
+
 <style lang="scss">
 .box {
   background-color: var(--bs-tertiary-bg);
