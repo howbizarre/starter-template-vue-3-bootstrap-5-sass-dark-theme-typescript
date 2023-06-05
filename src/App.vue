0f454d82fc7375bec406218836e7c3f1e1beeeb9
How Bizarre <howbizarre@gmail.com>
2023-06-05 23:36:56 +0300

    UPD: All to the latest version DEL: LogRocket for errors in new version VER: 1.1.2

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,6 @@
   <RouterView />
 </template>
 
-<script lang="ts" setup>
-import LogRocket from 'logrocket';
-LogRocket.init(`${import.meta.env.VITE_LOGROCKET_ID}`);
-</script>
-
 <style lang="scss">
-.box {
-  background-color: var(--bs-tertiary-bg);
-}
+.box { background-color: var(--bs-tertiary-bg); }
 </style>
